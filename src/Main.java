70276294376ef605506485f195ab18a37bf4981d
stormlin <376997313@qq.com>
2016-11-28 19:16:11 +0800

    ReverseString AC

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     public static void main(String []args){
 
-        PowerofTwo powerofTwo=new PowerofTwo();
-        powerofTwo.Show(1073741825);
+        ReverseString reverseString=new ReverseString();
+        reverseString.Show("Hello");
 
     }
 
---
+++ src/ReverseString.java
@@ -0,0 +1,23 @@
+/**
+ * Created by K9A2S on 2016/11/28.
+ */
+
+/**
+ * Create by Stormlin. All rights reserved.
+ **/
+
+public class ReverseString {
+
+    public void Show(String input){
+        System.out.println(reverseString(input));
+    }
+
+    private String reverseString(String s){
+        StringBuilder sb=new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+}
