afd017b7edaf99cdccc9d5c3c82cc5b92b158a44
stormlin <376997313@qq.com>
2017-01-16 18:06:27 +0800

    HammingDistance AC

---
+++ src/HammingDistance.java
@@ -0,0 +1,21 @@
+/**
+ * Created by K9A2S on 2017/1/16.
+ */
+
+/**
+ * Create by Stormlin. All rights reserved.
+ **/
+
+public class HammingDistance {
+
+    public void Show(int x,int y) {
+
+    }
+
+    private int hammingDistance(int x, int y) {
+
+        return Integer.bitCount(x^y);
+
+    }
+
+}
