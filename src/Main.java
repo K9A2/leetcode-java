3295ff46481d0de24b284830f97ee735aa2dff90
stormlin <376997313@qq.com>
2017-04-20 20:36:10 +0800

    Added solution for "492. Construct the Rectangle".

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import easy.DetectCapital;
-
 public class Main {
 
     public static void main(String[] args) {
-        DetectCapital dc = new DetectCapital();
-        if (!dc.detectCapitalUse("USA")) {
-            return;
-        }
-        System.out.print("USA");
+
+
+
     }
 
 }
