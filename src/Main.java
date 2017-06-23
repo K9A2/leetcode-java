25edeea1fe662ad3252cb7596a2f085c8c851d17
stormlin <lin-jinting@outlook.com>
2017-06-23 20:09:16 +0800

    Finished "1. Two Sum". @20:08 2017-06-23.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import easy.Array_Partition_One;
 import easy.Reverse_Words_in_a_String_III;
+import easy.TwoSum;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 4, 3, 2};
+        int[] nums = {3, 2, 4};
 
-        System.out.println(Array_Partition_One.arrayPairSum(nums));
+        System.out.println(Arrays.toString(TwoSum.twoSum(nums, 6)));
 
     }
 
