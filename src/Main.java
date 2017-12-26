99e3802b3dd437637d78e4639046c6021ef6d313
Lin Jinting <lin-jinting@outlook.com>
2017-12-26 09:38:37 +0800

    sync

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(JudgeRouteCircle.judgeCircle("LL"));
+        System.out.println(SelfDividingNumbers.selfDividingNumbers(1, 22));
 
     }
 
---
+++ src/easy/SelfDividingNumbers.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.List;
+
+/**
+ * Author: stormlin
+ * Personal Website: www.stormlin.com
+ * Problem Name: 728. Self Dividing Numbers
+ * Problem Address: https://leetcode.com/problems/self-dividing-numbers/description/
+ * Solution Date: Dec. 26, 2017
+ * -----------------------------------------------------------------------------
+ * A self-dividing number is a number that is divisible by every digit it contains.
+ *
+ * For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+ *
+ * Also, a self-dividing number is not allowed to contain the digit zero.
+ *
+ * Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+ * -----------------------------------------------------------------------------
+ * Example:
+ * Input:
+ *  left = 1, right = 22
+ * Output:
+ *  [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+ * -----------------------------------------------------------------------------
+ *  Note: The boundaries of each input argument are 1 <= left <= right <= 10000.
+ */
+
+public class SelfDividingNumbers {
+
+    public static List<Integer> selfDividingNumbers(int left, int right) {
+
+
+
+    }
+
+}
